52a2cabd9266e0bef96f824e5d6f5cc3803bc0a5
rayug <rayuga91@gmail.com>
2022-11-30 18:22:25 +0100

    Ejercicio JS Hecho falata css y corregir erroeres

--- JS/UT5_Practica2/UT5_Practica2.js
+++ JS/UT5_Practica2/UT5_Practica2.js
@@ -16,13 +16,45 @@ function cogerDatosSelectDatalist() {
     console.log(mensaje);
     //Datalist
     var getInputValue = document.getElementById("getValueDatalist");
+
     mensaje.innerHTML+=  "El dato del adatalist es: " + getInputValue.value + ".<br> Las diferencias que hay son:"
     + " <br> En el datalist para coger el dato necesitas ya de por si un input de entrada, entonces con coger el valor " +
         "de lo que selecciones en el input, ya coges el valor. <br>Mientras que en el select no hace falta un input para " +
         "coger el dato seleccionado";
 }
 
+
 function disableSelect(){
+    var selectDisabled = document.getElementById("selectDisabled");
+
+    var grupoDisabled = recuperarSeleccionados();
+
+    selectDisabled.disabled = false;
+    if (grupoDisabled.length == 0){
+        selectDisabled.disabled = true;
+    }
+
+    var inputs = selectDisabled.getElementsByTagName("optgroup");
+    for(var i = 0; i < inputs.length; i++) {
+        inputs[i].disabled = false;
+    }
+
+    for (var i = 0; i < grupoDisabled.length; i++){
+        document.getElementById(grupoDisabled[i]).disabled = true;
+    }
+
+
+
+}
+
+function recuperarSeleccionados(){
     var selectChoseDisableGroup = document.getElementById("selectChoseDisableGroup");
-    var selectDisabled = document.getElementById("selectDiabled");
+    var grupoDisabled = new Array();
+    for (var i = 0, iLen = selectChoseDisableGroup.options.length; i < iLen; i++) {
+        if (selectChoseDisableGroup.options[i].selected) {
+            grupoDisabled.push(selectChoseDisableGroup.options[i].value);
+            console.log(grupoDisabled);
+        }
+    }
+    return grupoDisabled;
 }
