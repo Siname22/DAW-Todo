f6fa37b297530a8dd5729b72d3a17a8ffde3b6e6
rayug <rayuga91@gmail.com>
2023-01-14 15:12:56 +0100

    ejercicio js hehco falta doc js

--- JS/UT6_Practica1/UT6_Practica1.js
+++ JS/UT6_Practica1/UT6_Practica1.js
@@ -3,13 +3,13 @@ window.addEventListener("load", iniciar);
 function iniciar(){
 
     var cambiaTexto = document.getElementById("cambiaTextos");
-    verCookie.addEventListener("click",cambiarTextos);
+    cambiaTexto.addEventListener("click",cambiarTextos);
 
     var cambiaClase = document.getElementById("cambiaClases");
-    crearCookie.addEventListener("click",cambiarClases);
+    cambiaClase.addEventListener("click",cambiarClases);
 
     var quitaClase = document.getElementById("quitaClases");
-    modificarCookie.addEventListener("click",quitarClases);
+    quitaClase.addEventListener("click",quitarClases);
 
 }
 
@@ -17,16 +17,53 @@ function iniciar(){
 
 
 function cambiarTextos(){
-    document.getElementById("parrafo1").innerHTML = "Primer parrafo cambiado"
+    var primerParrafo = document.getElementById("parrafo1");
+    primerParrafo.innerHTML = "Primer parrafo cambiado";
     //Cojo el dom y lo guadro en var tipo array. cambia la pos2 texto
-    document.getElementsByTagName("p")
+    var arrayParrafo = new Array();
+    arrayParrafo = document.getElementsByTagName("p");
+    console.log(arrayParrafo);
+
+    arrayParrafo[1].innerHTML = "Texto cambiando por bytagName";
+
+    var arrayParrafoClassName = new Array();
+    arrayParrafoClassName = document.getElementsByClassName("miclase");
+    console.log(arrayParrafoClassName);
+    arrayParrafoClassName[0].innerHTML="Texto cambiado con color rojo y por byClassName";
 }
 
 
 function cambiarClases(){
+    var primerParrafo = document.getElementById("parrafo1");
+    primerParrafo.setAttribute("class", "miclase");
 
+    //Segunda parte
+    var segundoParrafo = document.getElementById("parrafo2");
+    segundoParrafo.setAttribute("class", "miclase");
+
+    var arrayParrafoClassName = new Array();
+    arrayParrafoClassName = document.getElementsByClassName("miclase");
+
+    console.log(arrayParrafoClassName);
 }
 
 function quitarClases(){
+    /*
+    //Quitar la clase de parrafo1
+    var primerParrafo = document.getElementById("parrafo1");
+    primerParrafo.setAttribute("class", "");
 
+    //Segunda parte con ClassNAme
+    var arrayParrafoClassName = new Array();
+    arrayParrafoClassName = document.getElementsByClassName("miclase");
+
+    console.log(arrayParrafoClassName);
+    arrayParrafoClassName[0].setAttribute("class","");
+    arrayParrafoClassName[0].setAttribute("class","");
+    */
+
+    var arrayParrafosTagName = document.getElementsByTagName("p");
+    for (var i = 0; i < arrayParrafosTagName.length; i++) {
+        arrayParrafosTagName[i].setAttribute("class", "");
+    }
 }
